6b755b5bd6f3a283e4ba104987f3b12746dcf601
Rakin <rakchy1@gmail.com>
2023-10-14 02:43:52 +0100

    Creates a function for making a guess

--- milestone_2.py
+++ milestone_2.py
@@ -2,10 +2,12 @@ import random
 word_list = ["mango", "pineapple", "apple", "melon", "guava"]
 word = random.choice(word_list)
 
-guess = input("Guess a letter\n")
-if len(guess)==1 and guess.isalpha()==True:
-    print("Good guess!")
-else:
-    print("Oops! That is not a valid input.")
+def make_guess():
+    guess = input("Guess a letter\n")
+    if len(guess)==1 and guess.isalpha()==True:
+        print("Good guess!")
+    else:
+        print("Oops! That is not a valid input.")
 
+make_guess()
 print(word)
